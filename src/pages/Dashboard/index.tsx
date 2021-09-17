194f4709ec6d44f89995a2721067d1034ed7bc9e
FilipeCavalcante <filipencavalcante@gmail.com>
2021-09-17 13:17:08 -0300

    Starting to update the app design

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
 
-import { Title } from './styles';
+import { Title, Form, Repositories } from './styles';
 
-const Dashboard: React.FC = () => <Title>Explore repositórios no Github</Title>;
+import logoImg from '../../assets/logo.svg';
 
+const Dashboard: React.FC = () => (
+  <>
+    <img src={logoImg} alt="GitHub Explorer" />
+    <Title>Explore Github Repositories</Title>
+    <Form>
+      <input type="text" placeholder="Type the repository name" />
+      <button type="submit">SEARCH</button>
+    </Form>
+    <Repositories>
+      <a href="teste">
+        <img
+          src="https://avatars.githubusercontent.com/u/18223265?v=4"
+          alt="Perfil"
+        />
+        <div>
+          <strong>/repository/test</strong>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
+        </div>
+      </a>
+    </Repositories>
+  </>
+);
 export default Dashboard;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Dashboard from '../pages/Dashboard';
+// import MyComponent from '../pages/MyComponent';
 import Repository from '../pages/Repository';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Dashboard} />
+    {/* <Route path="/" exact component={MyComponent} /> */}
     <Route path="/repo" component={Repository} />
   </Switch>
 );
